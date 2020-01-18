7d2b38e2879bfcfcbb09508f659b59b852346f5d
Bonggar <bonggarsitumorang@gmail.com>
2020-01-18 20:58:23 +0700

    add routes, implement in main

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-	"time"
+
+	"github.com/bonggar/gorestapi/database"
+	"github.com/bonggar/gorestapi/router"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
-}
-
 func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	// Open connection to database
+	database.SQLiteDBConnect()
+	defer database.GetDB().Close()
+
+	// Register routes
+	routeHandler := router.Make()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: routeHandler,
+	}
+
+	// service connections
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("listen: %s\n", err)
+	}
+
+	// Ready to serve
+	log.Println("listen on", srv.Addr)
 }
---
+++ router/router.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"github.com/bonggar/gorestapi/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+//Cors : handle client origin rules
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Next()
+	}
+}
+
+//Make : create endpoints
+func Make() *gin.Engine {
+	//Default : with the Logger and Recovery middleware already attached
+	r := gin.Default()
+
+	//Applying CORS rule
+	r.Use(Cors())
+
+	//Grouping to Version 1.0
+	v1 := r.Group("api/v1")
+	{
+		v1.GET("/users", service.GetUsers)
+		v1.GET("/users/:id", service.GetUser)
+		v1.POST("/users", service.CreateUser)
+		v1.PUT("/users/:id", service.UpdateUser)
+		v1.DELETE("/users/:id", service.DeleteUser)
+		v1.OPTIONS("/users", service.OptionsUser)
+		v1.OPTIONS("/users/:id", service.OptionsUser)
+	}
+
+	return r
+}
