664eec0e8782d75746cc9d996a9e17fcf1c7a896
Bonggar <bonggarsitumorang@gmail.com>
2020-01-19 19:06:05 +0700

    add static pages build with react js

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bonggar/gorestapi
 go 1.13
 
 require (
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
 	github.com/gin-gonic/gin v1.5.0
 	github.com/jinzhu/gorm v1.9.12
 	github.com/joho/godotenv v1.3.0
---
+++ view/precache-manifest.b9ea46714e32a9a463db7148f64ad407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "427fb96f1dc5887761ae",
+    "url": "/static/css/main.895d69a5.chunk.css"
+  },
+  {
+    "revision": "427fb96f1dc5887761ae",
+    "url": "/static/js/main.0b7d4f51.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb6fd61a30a89474eed9",
+    "url": "/static/css/2.7a3d8116.chunk.css"
+  },
+  {
+    "revision": "bb6fd61a30a89474eed9",
+    "url": "/static/js/2.12950859.chunk.js"
+  },
+  {
+    "revision": "ef587d850b6bcca934c3b9f6ed1fba51",
+    "url": "/index.html"
+  }
+];
